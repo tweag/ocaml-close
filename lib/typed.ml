c54af0d217785768bad0fa9ad77f2a8702effcfb
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-07 04:24:03 +0100

    Investigate types a bit more

--- lib/typed.ml
+++ lib/typed.ml
@@ -1,6 +1,8 @@
 (*open Base*)
 
-let type_ast = Typemod.type_structure Env.empty
+let type_ast s =
+  let unstable = Ppxlib_ast.Selected_ast.To_ocaml.copy_structure s in
+  Typemod.type_structure Env.empty unstable
 
 (* can be untyped if needed with untypeast *)
 
@@ -10,3 +12,5 @@ let type_ast = Typemod.type_structure Env.empty
 (* This way we could possibly avoid having to use merlin *)
 
 (* However we have to investigate version stability when using this *)
+
+(* we have to somehow fill the env with existing libraries :( *)
