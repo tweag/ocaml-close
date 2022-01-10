000944378e76710ca1ad5a51b97ce254762cca37
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-10 20:11:34 +0100

    Add verbose parameter

--- lib/close.mli
+++ lib/close.mli
@@ -1,6 +1,6 @@
 
-val analyse : string -> (unit, string) result
+val analyse : string -> bool -> (unit, string) result
 (** Analyse opens of a file and prints suggestions for change. *)
 
-val filtered_analyse : string -> (unit, [`Msg of string]) result
+val filtered_analyse : string -> bool -> (unit, [`Msg of string]) result
 (** Same as {!analyse} but errors have a more standard format. *)
