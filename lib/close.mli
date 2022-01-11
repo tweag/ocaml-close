9a4d9a61c57fb185a1f168741078e8cac06d99ab
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-12 00:24:35 +0100

    Add note

--- lib/close.mli
+++ lib/close.mli
@@ -5,3 +5,8 @@ type args = {
 
 val execute : args -> string list -> (unit, [`Msg of string]) result
 (** Analyse opens of a file and prints suggestions for change. *)
+
+(* TODO : have different commands
+    lint to recommend modifications (and save this in a temp file)
+    fix to apply the listed modifications
+*)
