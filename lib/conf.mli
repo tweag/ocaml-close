301c23a58567c24fdcf64f286d00179e762427b1
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-13 19:37:54 +0100

    Encode decision making as configuration file

--- lib/conf.mli
+++ lib/conf.mli
@@ -1,7 +1,21 @@
-open Utils
+type rule =
+  | And of rule list
+  | Or of rule list
+  | Not of rule
+  | True
+  | False
+  | Min_use of int
+  | Min_exported of int
+  | In_list of string list
+  | Exports_syntax
+  | Exports_modules
+  | Exports_modules_only
+
+type rule_kind = Keep | Remove | Convert_to_local | Move
 
 type conf = {
-  keep_rule : keep_rule;
+  rules : (rule_kind * rule) list;
+  precedence : rule_kind list;
 }
 
 val read_conf : ?conf_file:string -> unit -> conf
