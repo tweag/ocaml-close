5b21a8cb570f1c7cb105dff98d5ce170e9b2c9cf
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-14 23:03:32 +0100

    Remove now useless syntactic module

--- lib/syntactic.mli
+++
@@ -1,3 +0,0 @@
-val get_source_fragment : string list -> Utils.pos -> Utils.pos -> (string, string) result
-(** [get_source_fragment filename start finish] returns the string in the file
-    delimited by the two positions. *)
