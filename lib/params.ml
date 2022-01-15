1846dd88ceaf938e26101b839fd0657b89397ad2
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-15 23:03:04 +0100

    Add missing param file

---
+++ lib/params.ml
@@ -0,0 +1,45 @@
+module Log = struct
+  type t = {
+    change : string -> unit;
+    new_file : string -> unit;
+    debug : string -> unit;
+  }
+
+  let change kind txt = match kind with
+    | `Text -> Stdio.printf "%s...\n%!" txt
+    | `None -> ()
+    | `Bar (f, _) -> f txt
+
+  let new_file total kind =
+    let n = ref 0 in
+    fun name -> incr n;
+      match kind with
+      | `Text -> Stdio.printf "Processing %s (%d/%d)\n%!" name !n total
+      | `None -> ()
+      | `Bar (_, f) -> f 1
+
+  let debug kind txt = match kind with
+    | `Text -> Stdio.printf "%s\n%!" txt
+    | _ -> ()
+
+  let make kind (fs, total) =
+    let kind = match kind with
+      | `Text -> `Text
+      | `None -> `None
+      | `Bar -> `Bar fs
+    in {change = change kind; new_file = new_file total kind;
+        debug = debug kind}
+end
+
+
+type t = {
+  conf : Conf.conf;
+  skip_absent : bool;
+  silence_errors : bool;
+  log : Log.t;
+}
+
+let of_args (t : Utils.args) info =
+  let conf = Conf.read_conf ?conf_file:t.conf_file () in
+  {conf; skip_absent = t.skip_absent; silence_errors = t.silence_errors;
+   log = Log.make t.report info}
