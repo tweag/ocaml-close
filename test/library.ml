8b4ad97375422c21ba8cd652ef0ca59071ea0202
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-18 23:14:43 +0100

    Add stub modules to the tests

--- test/library.ml
+++ test/library.ml
@@ -1,14 +1,23 @@
 module A = struct
   let x = ()
   let y = ()
+  module S = struct
+    let s = ()
+  end
 end
 
 module B = struct
   let y = ()
   let z = ()
+  module S = struct
+    let s = ()
+  end
 end
 
 module C = struct
   let z = ()
   let a = ()
+  module S = struct
+    let s = ()
+  end
 end
