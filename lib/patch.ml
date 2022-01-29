11e0cac4367cf1cb4de8dc84a8d597995ecd4f59
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-29 03:17:58 +0100

    Validate patches

--- lib/patch.ml
+++ lib/patch.ml
@@ -5,18 +5,28 @@ type single =
   | Insert of {what : string; where : pos; newline : bool}
 [@@deriving show]
 
-type t = {actions : single list; filename : string}
+type t =
+  | Valid of {actions : single list; filename : string}
+  | Invalid of string
 [@@deriving show]
 
-let delete ~chunk t =
-  {t with actions = (Delete chunk) :: t.actions}
+let delete ~chunk = function
+  | Invalid s -> Invalid s
+  | Valid t ->
+    if chunk.ch_begin.line = chunk.ch_end.line then
+      Valid {t with actions = (Delete chunk) :: t.actions}
+    else Invalid "cannot delete chunk of multiple lines"
 
-let insert ?(newline=false) ~at what t =
-  {t with actions = (Insert {what; where=at; newline}) :: t.actions}
+let insert ?(newline=false) ~at what = function
+  | Invalid s -> Invalid s
+  | Valid t ->
+    Valid {t with actions = (Insert {what; where=at; newline}) :: t.actions}
 
-let empty filename = {actions = []; filename}[@@deriving show]
+let empty filename = Valid {actions = []; filename}
 
-let is_empty t = Base.List.is_empty t.actions
+let invalid why = Invalid why
+
+let is_empty = function Valid t -> Base.List.is_empty t.actions | _ -> true
 
 (* TODO: when applying insert, DO NOT apply if string already exist at this
  * place *)
