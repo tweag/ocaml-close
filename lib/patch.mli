63f7fbcd9e4b874cfe517a2c0b9fa1f4bb283161
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-29 03:24:36 +0100

    MLI file for patch

---
+++ lib/patch.mli
@@ -0,0 +1,12 @@
+type t
+[@@deriving show]
+
+val empty : string -> t
+
+val invalid : string -> t
+
+val is_empty : t -> bool
+
+val delete : chunk:Utils.chunk -> t -> t
+
+val insert : ?newline:bool -> at:Utils.pos -> string -> t -> t
