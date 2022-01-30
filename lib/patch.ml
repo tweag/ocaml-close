c672ac89a34dc3c840c57cd29f0eda87168bfed4
Virgile Robles <virgile.robles@protonmail.ch>
2022-01-31 00:16:24 +0100

    Text edition is now more correct

--- lib/patch.ml
+++ lib/patch.ml
@@ -39,29 +39,33 @@ let is_empty = function Valid t -> Base.List.is_empty t.actions | _ -> true
 
 open Base
 
-let apply_single lines = function
+let apply_single (lines : Piece_table.t Array.t) = function
   | Delete chunk ->
     let line_no = chunk.ch_begin.line in
     let line = lines.(line_no - 1) in
-    let before = String.prefix line chunk.ch_begin.col in
-    let after = String.drop_prefix line chunk.ch_end.col in
-    lines.(line_no - 1) <- before ^ after
+    let length = chunk.ch_end.col - chunk.ch_begin.col in
+    Piece_table.delete_many_orig  ~pos:chunk.ch_begin.col ~length line
   | Insert {what; where; newline} ->
     let line_no = where.line in
     let line = lines.(line_no - 1) in
-    let before = String.prefix line where.col in
-    let after = String.drop_prefix line where.col in
-    let final = if newline then
-        before ^ what ^ "\n" ^ (String.make (String.length before) ' ') ^ after
-      else before ^ what ^ after
+    let to_insert = if newline then
+        what ^ "\n" ^ (String.make where.col ' ')
+      else what
     in
-    lines.(line_no - 1) <- final
+    Piece_table.insert ~orig_pos:true ~pos:where.col to_insert line
+    |> ignore
 
 let apply = function
   | Invalid s -> Result.failf "Couldn't apply an invalid patch: %s" s
   | Valid {actions; filename} ->
     let out_filename = filename ^ ".suggested.ml" in
-    let lines = Array.of_list @@ Stdio.In_channel.read_lines filename in
+    let lines =
+      Stdio.In_channel.read_lines filename
+      |> Array.of_list
+      |> Array.map ~f:Piece_table.create
+    in
     List.iter ~f:(apply_single lines) actions;
-    Stdio.Out_channel.write_lines out_filename (Array.to_list lines);
+    Array.to_list lines
+    |> List.map ~f:Piece_table.contents
+    |> Stdio.Out_channel.write_lines out_filename;
     Result.return ()
